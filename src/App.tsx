7b1db9c85a7f851f0f8d21e41004eb81051229c8
GustavoEdson <ggugu9289@gmail.com>
2025-04-21 17:43:47 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import { Nav } from "./components/Nav";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
 import { Project } from "./components/Project";
+import { motion } from "motion/react";
 
 function App() {
   return (
     <>
       <Nav logo="gustavo edson" />
-      <Home
-        name="gustavo edson"
-        text="A skilled web developer,
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Home
+          name="gustavo edson"
+          text="A skilled web developer,
               crafting and managing websites 
               and web applications to ensure 
               the success of the entire product with finesse."
-      />
+        />
+      </motion.div>
       <About />
-      <Project />
+      <Project />;
     </>
   );
 }
